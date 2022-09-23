dfe3db01e156920fc17a0879f3bffb8b7baa3b47
Sachin Shelar <40835629+sach-shelar@users.noreply.github.com>
2022-09-24 00:44:50 +0530

    Color changes

--- all_layouts/lib/column_example.dart
+++ all_layouts/lib/column_example.dart
@@ -11,12 +11,14 @@ class ColumnExample extends StatelessWidget {
         width: double.infinity,
         color: Colors.white,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(color: Color(0xFFF4BFBF), width: 200, height: 100),
             Container(color: Color(0xFFFFD9C0), width: 200, height: 100),
-            Container(color: Color(0xFFFAF0D7), width: 200, height: 100)
+            Expanded(
+                child: Container(
+                    color: Color(0xFFFAF0D7), width: 200, height: 100))
           ],
         ),
       ),
